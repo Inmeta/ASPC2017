9f650850d5c587271aa0a9ce0c1765a67b4c0029
Peder Sundbø <peder.sundbo@inmeta.no>
2017-02-03 14:43:22 +0100

    Updates in files, script and provision

--- ASPC/SiteAssets/JS/aspc.scripts.js
+++ ASPC/SiteAssets/JS/aspc.scripts.js
@@ -14,16 +14,20 @@ ASPC.Rollup = {
 
     MenuItem: function (data) {
         var self = this;
-        self.title = data.title;
+        self.title = data.Title;
+        self.location = data.ASPC_Location;
+        self.members = data.ASPC_Members;
+        self.type = data.ASPC_RebelType;
+        self.url = data.__metadata.uri;
     },
 
     RollupViewModel: function (menuItems) {
         var self = this;
-        self.jsondata = 
-        self.menuItems = self.jsondata.children;
+        self.jsondata = ASPC.Rollup.RebelsData;
+        self.menuItems = self.jsondata.d.results;
 
         self.load = function () {
-            var mappedItems = $.map(self.jsondata.children, function (item) { return new ASPC.Rollup.MenuItem(item) });
+            var mappedItems = $.map(self.jsondata.d.results, function (item) { return new ASPC.Rollup.MenuItem(item) });
             self.menuItems = mappedItems;
         }
 
@@ -33,16 +37,17 @@ ASPC.Rollup = {
     GetItems: function (elId) {
         $.ajax({
             url: "/_api/web/lists/getbytitle('Rebels')/getitems",
-            method: "GET",
+            method: "POST",
             headers: { "accept": "application/json;odata=verbose", "content-type": "application/json;odata=verbose", "X-RequestDigest": $("#__REQUESTDIGEST").val() },
-            success: function (data) {
+            data: JSON.stringify({ 'query': { '__metadata': { 'type': 'SP.CamlQuery' }, 'ViewXml': '<View><Query></Query></View>' } }),
+            success: function (data) {   
                 console.log(data);
-                ASPC.Rollup.RebelsData = data;
+                ASPC.Rollup.RebelsData = data;                
                 ASPC.Rollup.InitRollup(elId);
             },
             error: function (data) {
                 console.log(data);
             }
         });
-    },
+    },    
 };
