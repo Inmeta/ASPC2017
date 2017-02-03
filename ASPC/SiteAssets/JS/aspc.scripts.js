01a2b367e2c22128c48e82bc6792055039635a0e
Peder Sundbø <peder.sundbo@inmeta.no>
2017-02-03 13:03:08 +0100

    Added knockout in script refactored disp tamplate

--- ASPC/SiteAssets/JS/aspc.scripts.js
+++ ASPC/SiteAssets/JS/aspc.scripts.js
@@ -1,9 +1,15 @@
 var ASPC = ASPC || {};
 
 ASPC.Rollup = {
+    RebelsData: "",
+
+    Init: function(elId){
+        ASPC.Rollup.GetItems(elId);
+    },
+
     InitRollup: function (rollID) {
         var roll = document.getElementById(rollID);
-        ko.applyBindings(new Coop.TopMenu.TopMenuViewModel({ elementId: rollID }), roll);
+        ko.applyBindings(new ASPC.Rollup.RollupViewModel({ elementId: rollID }), roll);
     },
 
     MenuItem: function (data) {
@@ -13,7 +19,7 @@ ASPC.Rollup = {
 
     RollupViewModel: function (menuItems) {
         var self = this;
-        self.jsondata = //GET JSON
+        self.jsondata = 
         self.menuItems = self.jsondata.children;
 
         self.load = function () {
@@ -22,5 +28,21 @@ ASPC.Rollup = {
         }
 
         self.load();
+    },
+
+    GetItems: function (elId) {
+        $.ajax({
+            url: "/_api/web/lists/getbytitle('Rebels')/getitems",
+            method: "GET",
+            headers: { "accept": "application/json;odata=verbose", "content-type": "application/json;odata=verbose", "X-RequestDigest": $("#__REQUESTDIGEST").val() },
+            success: function (data) {
+                console.log(data);
+                ASPC.Rollup.RebelsData = data;
+                ASPC.Rollup.InitRollup(elId);
+            },
+            error: function (data) {
+                console.log(data);
+            }
+        });
     },
 };
